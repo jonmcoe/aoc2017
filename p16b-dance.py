341a8a7471500be985473c54a46b48ee2ea8aadf
Jon Coe <jon@seatgeek.com>
2017-12-16 10:27:54 -0500

    p16b done. inefficient

--- p16b-dance.py
+++ p16b-dance.py
@@ -5,6 +5,7 @@ from shared import get_moves, CHARACTER_TO_FUNCTION
 
 
 DEFAULT_NUMBER_OF_DANCERS = 16
+NUMBER_OF_DANCE_CYCLES = 10 ** 9
 
 
 if __name__ == '__main__':
@@ -12,11 +13,12 @@ if __name__ == '__main__':
     number_of_dancers = (len(sys.argv) > 2 and int(sys.argv[2])) or DEFAULT_NUMBER_OF_DANCERS
     debug = len(sys.argv) > 3 and bool(sys.argv[3])
     current_dancers = list(string.ascii_lowercase[:number_of_dancers])
-    for move in move_set:
-        move_func = CHARACTER_TO_FUNCTION[move[0]]
-        move_args = move[1:].split('/')
-        if debug:
-            print(''.join(current_dancers))
-            print("MOVE: {0}".format(move))
-        current_dancers = move_func(current_dancers, *move_args)
+    for _ in range(NUMBER_OF_DANCE_CYCLES):
+        for move in move_set:
+            move_func = CHARACTER_TO_FUNCTION[move[0]]
+            move_args = move[1:].split('/')
+            if debug:
+                print(''.join(current_dancers))
+                print("MOVE: {0}".format(move))
+            current_dancers = move_func(current_dancers, *move_args)
     print(''.join(current_dancers))
--- shared.py
+++ shared.py
@@ -1,3 +1,6 @@
+# 1
+
+
 def get_string(filename):
     with open(filename, 'r') as f:
         raw_contents = f.read()
@@ -15,3 +18,40 @@ def get_circle_sum(sumstring, counterpart_func, debug=False):
             print(running_sum)
             print(sumstring[i:])
     return running_sum
+
+
+#2
+
+
+def spin(dancers, step):
+    step = int(step)
+    return [dancers[i - step] for i, _ in enumerate(dancers)]
+
+
+def _swap(dancers, first_index, second_index):
+    x = list(dancers)
+    x[first_index] = dancers[second_index]
+    x[second_index] = dancers[first_index]
+    return x
+
+
+def exchange(dancers, first_index, second_index):
+    first_index = int(first_index); second_index = int(second_index)
+    return _swap(dancers, first_index, second_index)
+
+
+def partner(dancers, first_name, second_name):
+    return _swap(dancers, dancers.index(first_name), dancers.index(second_name))
+
+
+CHARACTER_TO_FUNCTION = {
+    's': spin,
+    'x': exchange,
+    'p': partner
+}
+
+
+def get_moves(filename):
+    with open(filename, 'r') as f:
+        raw_contents = f.read()
+    return raw_contents.split(',')
