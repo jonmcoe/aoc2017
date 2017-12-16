ddb2d767a7a985a1a6e4ac6d06f1d2671df5fe13
Jon Coe <jon@seatgeek.com>
2017-12-16 17:44:13 -0500

    add note

--- p03a-spiral.py
+++ p03a-spiral.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
         # TODO: conditional for whether in growing or shrinking section. deal with corners
         # right now we are only correct for those "slightly" less (within `block / 2` than the odd integer's square (bottom edge)
         # we'll see if this is worth fixing when we see part two
+        # NOTE: probably not worth fixing. I see no way around a true constructive approach in p03b
         steps -= 1
         loc -= 1
     print(steps)
