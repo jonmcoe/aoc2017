d721f784c41a054a06b0c2a9f88116d0b33c6a3d
Jon Coe <jon@seatgeek.com>
2017-12-22 08:08:09 -0500

    p21 refactor and speedup. getting wrong answer for p12b still

--- p21a-enhance.py
+++ p21a-enhance.py
@@ -1,4 +1,5 @@
 import sys
+from functools import lru_cache
 
 import shared
 
@@ -10,13 +11,15 @@ def parse_rule(l):
 
 def parse_rules(lines_in):
     return {
-        k:v for k,v in (parse_rule(l) for l in lines_in)
+        k: v for k, v in (parse_rule(l) for l in lines_in)
     }
 
 
-# TODO: we can get away with a lot cheaper bc there are only two elements
-# or generate all of these once in the beginning
+# TODO: we can get away with a lot cheaper bc there are only two elements and therefore symmetries.
+# then again cache makes less unimportant
+@lru_cache()
 def make_transformations(x):
+
     def horizontal_mirror(x):
         return x[::-1]
 
@@ -34,58 +37,36 @@ def make_transformations(x):
         counterclockwise_turn(counterclockwise_turn(x)),
         counterclockwise_turn(counterclockwise_turn(counterclockwise_turn(x))),
         counterclockwise_turn(horizontal_mirror(x)),
-        counterclockwise_turn(vertical_mirror(x)),
+        counterclockwise_turn(vertical_mirror(x))
     ]
 
 
-def convert_blocks_to_picture(blocks_grid, block_size):
-    return tuple(''.join(blocks_grid[i // block_size][j][i % block_size] for j, _ in enumerate(blocks_grid)) for i in range(len(blocks_grid) * block_size))
+def convert_blocks_to_picture(blocks_grid):
+    block_size = len(blocks_grid[0][0])
+    return tuple(
+        ''.join(blocks_grid[i // block_size][j][i % block_size] for j, _ in enumerate(blocks_grid))
+        for i in range(len(blocks_grid) * block_size)
+    )
 
 
 def perform_iteration(p):
-    if len(p) % 2 == 0:
 
-        three_by_three_blocks = []
-        for i in range(len(p) // 2):
-            this_row = [None] * (len(p) // 2)
-            three_by_three_blocks.append(this_row)
-        for i in range(len(p) // 2):
-            for j in range(len(p) // 2):
-                cur_block = tuple(r[2 * i: 2 * i + 2] for r in p[j * 2:2 * j + 2])
-                transformations = make_transformations(cur_block)
-                for t in transformations:
-                    if t in ruleset:
-                        cur_three_block = ruleset[t]
-                        # print("apply for: {0} == {1} --> {2}  ({3}, {4})".format(cur_block, t, ruleset[t], i, j))
+    chop_by = next(i for i in (2, 3) if len(p) % i == 0)
+    num_blocks = len(p) // chop_by
+    blocks_grid = []
+    for _ in range(num_blocks):
+        this_row = [None] * num_blocks
+        blocks_grid.append(this_row)
 
-                        break
-                else:
-                    raise Exception   # TODO: StopIteration
-                three_by_three_blocks[i][j] = cur_three_block
-        return convert_blocks_to_picture(three_by_three_blocks, 3)
+    for i in range(num_blocks):
+        for j in range(num_blocks):
+            cur_block_in = tuple(r[chop_by * i: chop_by * i + chop_by] for r in p[j * chop_by:chop_by * j + chop_by])
+            transformation = next(t for t in make_transformations(cur_block_in) if t in ruleset)
+            cur_block_out = ruleset[transformation]
+            # print("apply for: {0} == {1} --> {2}  ({3}, {4})".format(cur_block_in, transformation, cur_block_out, i, j))
+            blocks_grid[i][j] = cur_block_out
+    return convert_blocks_to_picture(blocks_grid)
 
-    if len(p) % 3 == 0:
-
-        four_by_four_blocks = []
-        for i in range(len(p) // 3):
-            this_row = [None] * (len(p) // 3)
-            four_by_four_blocks.append(this_row)
-
-
-        for i in range(len(p) // 3):
-            for j in range(len(p) // 3):
-                cur_block = tuple(r[3 * i: 3 * i + 3] for r in p[j * 3:3 * j + 3])
-                transformations = make_transformations(cur_block)
-                for t in transformations:
-                    if t in ruleset:
-                        cur_four_block = ruleset[t]
-                        # print("apply for: {0} == {1} --> {2}  ({3}, {4})".format(cur_block, t, ruleset[t], i, j))
-                        break
-                else:
-                    raise Exception  # TODO: StopIteration
-                four_by_four_blocks[i][j] = cur_four_block
-
-        return convert_blocks_to_picture(four_by_four_blocks, 4)
 
 if __name__ == '__main__':
     lines_in = shared.get_exact_rows(sys.argv[1])
@@ -93,7 +74,7 @@ if __name__ == '__main__':
     picture = ('.#.', '..#', '###')
 
     cap = 19
-    for i in range(cap):
-        print("{0}: {1}".format(i, len([c for c in str(picture) if c == '#'])))
+    for iteration in range(cap):
+        print("{0}: {1}".format(iteration, len([c for c in str(picture) if c == '#'])))
         picture = perform_iteration(picture)
     print("{0}: {1}".format(cap, len([c for c in str(picture) if c == '#'])))
