29741e6eb36d4c79f0822d3568d8076bfc844832
Jon Coe <jon@seatgeek.com>
2017-12-23 22:32:29 -0500

    21b done. transposition bug fixed!

--- p21a-enhance.py
+++ p21a-enhance.py
@@ -49,15 +49,21 @@ def convert_blocks_to_picture(blocks_grid):
     )
 
 
-def perform_iteration(p):
-
-    chop_by = next(i for i in (2, 3) if len(p) % i == 0)
-    num_blocks = len(p) // chop_by
+def convert_picture_to_blocks(unified_grid):
+    chop_by = next(i for i in (2, 3) if len(unified_grid) % i == 0)
+    num_blocks = len(unified_grid) // chop_by
     blocks_grid = shared.initialize_2d_array(num_blocks, num_blocks, '.')
-
     for i in range(num_blocks):
         for j in range(num_blocks):
-            cur_block_in = tuple(r[chop_by * i: chop_by * i + chop_by] for r in p[j * chop_by:chop_by * j + chop_by])
+            cur_block_in = tuple(r[chop_by * j: chop_by * j + chop_by] for r in unified_grid[i * chop_by:chop_by * i + chop_by])
+            blocks_grid[i][j] = cur_block_in
+    return blocks_grid
+
+
+def perform_iteration(p):
+    blocks_grid = convert_picture_to_blocks(p)
+    for i, blocks_row in enumerate(blocks_grid):
+        for j, cur_block_in in enumerate(blocks_row):
             transformation = next(t for t in make_transformations(cur_block_in) if t in ruleset)
             cur_block_out = ruleset[transformation]
             # print("apply for: {0} == {1} --> {2}  ({3}, {4})".format(cur_block_in, transformation, cur_block_out, i, j))
@@ -65,12 +71,17 @@ def perform_iteration(p):
     return convert_blocks_to_picture(blocks_grid)
 
 
+def stringify_picture(p):
+    for r in p:
+        print(r)
+
+
 if __name__ == '__main__':
     lines_in = shared.get_exact_rows(sys.argv[1])
     ruleset = parse_rules(lines_in)
     picture = ('.#.', '..#', '###')
 
-    cap = 6
+    cap = 18
     for iteration in range(cap):
         print("{0}: {1}".format(iteration, len([c for c in str(picture) if c == '#'])))
         picture = perform_iteration(picture)
